a808f950ce4f719bc53a00bcb2ff2531767bc296
U0181792 <john.hunsley@thomsonreuters.com>
2018-11-14 17:19:14 +0000

    working example

--- src/main/java/com/johnhunsley/oauth2/AuthServerOAuth2Config.java
+++ src/main/java/com/johnhunsley/oauth2/AuthServerOAuth2Config.java
@@ -39,7 +39,7 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
         clients.inMemory().withClient("myClientId").secret(passwordEncoder().encode("secret"))
                 .authorizedGrantTypes("authorization_code").scopes("user_info")
                 .autoApprove(true)
-                .redirectUris("http://localhost:8082/client/login")
+                .redirectUris("http://localhost:8082/login")
         ;
     }
 
